0b1ef41bf8b718780accb3f2659b40def94cabbc
Pedro José Pereira Vieito <pvieito@gmail.com>
2018-02-11 22:53:16 +0100

    Updated Swift package manifest

--- Package.swift
+++ Package.swift
@@ -6,12 +6,9 @@ import PackageDescription
 let package = Package(
     name: "LoggerKit",
     products: [
-        .library(name: "LoggerKit", type: .static, targets: ["LoggerKit"]),
-    ],
-    dependencies: [
-        
+        .library(name: "LoggerKit", targets: ["LoggerKit"]),
     ],
     targets: [
-        .target(name: "LoggerKit", dependencies: [], path: "LoggerKit")
+        .target(name: "LoggerKit", path: "LoggerKit")
     ]
 )
