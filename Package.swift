187da36d2d335ac9538ea7e059d9ca4800ee6a53
Pedro José Pereira Vieito <pvieito@gmail.com>
2018-02-11 17:49:04 +0100

    Updated Package.swift to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,16 +1,18 @@
-//
-//  Logger.swift
-//  LoggerKit
-//
-//  Created by Pedro José Pereira Vieito on 15/1/17.
-//  Copyright © 2017 Pedro José Pereira Vieito. All rights reserved.
-//
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LoggerKit",
+    products: [
+        .library(name: "LoggerKit", type: .dynamic, targets: ["LoggerKit"]),
+        .library(name: "LoggerKit", type: .static, targets: ["LoggerKit"])
+    ],
+    dependencies: [
+
+    ],
     targets: [
-        Target(name: "LoggerKit"),
+        .target(name: "LoggerKit", dependencies: [], path: "LoggerKit")
     ]
 )
