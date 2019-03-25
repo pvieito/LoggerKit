b6b9bdabf240d3c2caffe53127661390866eb749
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-03-25 01:31:42 +0100

    Swift 5 + macOS Swift PM support

--- Package.swift
+++ Package.swift
@@ -5,9 +5,15 @@ import PackageDescription
 let package = Package(
     name: "LoggerKit",
     products: [
-        .library(name: "LoggerKit", targets: ["LoggerKit"]),
+        .library(
+            name: "LoggerKit",
+            targets: ["LoggerKit"]
+        )
     ],
     targets: [
-        .target(name: "LoggerKit", path: "LoggerKit")
+        .target(
+            name: "LoggerKit",
+            path: "LoggerKit"
+        )
     ]
 )
