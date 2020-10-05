835b41248dbe474eb33dac2c1ef1376dea7dc11d
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-10-05 16:02:06 +0200

    [LoggerKit] Added Windows support

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Rainbow", .branch("master"))
+        .package(url: "https://github.com/onevcat/Rainbow", from: "3.2.0")
     ],
     targets: [
         .target(
